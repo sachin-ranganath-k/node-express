6c28b36104ff5fdb1137b0ea79526dc48fa9b617
ranganathnk <ranganathnkulkarni@gmail.com>
2024-02-17 16:02:40 +0530

    get request

--- expressJs/app.js
+++ expressJs/app.js
@@ -10,18 +10,36 @@ app.get("/movies", (req, res) => {
   res.send("Movies");
 });
 
-//All the request params will be there in req.params:{} object -- params: { genre: 'comed', year: '2332' },
+//Params -- > All the request params will be there in req.params:{} object -- params: { genre: 'comed', year: '2332' },
 app.get("/movies/:genre/:year", (req, res) => {
   res.send(
     `Movie with Year - ${req.params.year} and Genre - ${req.params.genre}`
   );
 });
 
+//Query Strings --> http://localhost:3000/movie?name=sachin
+
+app.get("/movie", (req, res) => {
+  res.send(`Name ${req.query.name}, Age ${req.query.age}`); //returns the query string parameters in an object
+  console.log(req);
+});
+
+app.get("/moviesJson", (req, res) => {
+  let moviesList = [
+    {
+      movieName: "Apthamitra",
+      year: "2007",
+      genre: "Horror",
+    },
+    {
+      movieName: "Aptharakshaka",
+      year: "2011",
+      genre: "Horror",
+    },
+  ];
+  res.json(moviesList);
+});
+
 app.listen(3000, () => {
   console.log("Server is running...!");
 });
-
-/*Output
-Hello...
-Server is running...!
-*/
